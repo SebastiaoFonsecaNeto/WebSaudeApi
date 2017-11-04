849294c693aa6fe9ea452a8c01c869939ffb07e0
Sebastião Fonseca <sebastiao.fonseca.neto@gmail.com>
2017-11-04 15:45:35 -0200

    Ajuste map agenda
    Ajuste map agenda

---
+++ WebSaudeApi.Infra.Data/EntityConfig/AgendaConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using WebSaude.Domain.Entities;
+
+namespace WebSaude.Infra.Data.EntityConfig
+{
+    public class AgendaConfiguration : EntityTypeConfiguration<Agenda>
+    {
+        public AgendaConfiguration()
+        {
+            HasKey(c => c.Id);
+
+            Property(c => c.Id)
+                .IsRequired()
+                .HasColumnName("id");
+
+            Property(c => c.UnidadeId)
+                .IsRequired()
+                .HasColumnName("unidade_id");
+        }
+    }
+}
